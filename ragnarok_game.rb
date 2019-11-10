0312f1d05093a15b24faccc6febad4278a559895
Kenneth <rimorinkennet@gmail.com>
2019-11-10 16:17:14 +1100

    Name input

--- ragnarok_game.rb
+++ ragnarok_game.rb
@@ -1,7 +1,24 @@
 require 'artii'
 require 'colorize'
 
+input_name = true
+new_line = "\n"
+
 welcome = Artii::Base.new :font => 'slant'
 puts welcome.asciify('R a g n a r o k').colorize(:white)
 puts "- -" * 23 
 puts "#{" " * 10}Welcome to the world of chaos and epic battles"
+
+def red
+colorize(:red)
+end
+puts new_line
+while input_name
+    print "Plase enter name: "
+    name = gets.chomp.to_s
+    if name.length < 4 then puts "Please enter longer character name".red
+    else
+        break
+    end
+end
+
