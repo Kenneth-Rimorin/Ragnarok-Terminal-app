384ceab0f56d7c311c97f4b8d581ea33e2384b26
Kenneth <rimorinkennet@gmail.com>
2019-11-10 16:44:10 +1100

    Class Selection

--- ragnarok_game.rb
+++ ragnarok_game.rb
@@ -2,6 +2,7 @@ require 'artii'
 require 'colorize'
 
 input_name = true
+job_selection = true
 new_line = "\n"
 
 welcome = Artii::Base.new :font => 'slant'
@@ -12,13 +13,41 @@ puts "#{" " * 10}Welcome to the world of chaos and epic battles"
 def red
 colorize(:red)
 end
+def blue
+colorize(:blue)
+end
+def green
+    colorize(:green)
+end
+
 puts new_line
 while input_name
     print "Plase enter name: "
     name = gets.chomp.to_s
     if name.length < 4 then puts "Please enter longer character name".red
     else
+        puts "Welcome to Ragnarok #{name.blue}"
         break
     end
 end
-
+puts new_line
+while job_selection
+print "Please Select Class Job | 1 = Swordsman | 2 = Acolyte | 3 = Archer : "
+job = gets.chomp.to_i
+    if job == 1 
+        job = 1 
+        puts "Swordsman Selected".green
+        break
+    elsif job == 2
+        job =2 
+        puts "Acolyte Selected".green
+        break
+    elsif job == 3
+        job = 3 
+        puts "Archer Selected".green
+        break
+    else
+        puts "No Job Selected".red
+    end
+        
+end
