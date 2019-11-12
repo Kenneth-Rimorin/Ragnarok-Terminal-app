05a967f5e59ae0c4e5fa7b236c56b8efb2b64c3a
Kenneth <rimorinkennet@gmail.com>
2019-11-13 06:59:53 +1100

    Convo in prontera

--- ragnarok_game.rb
+++ ragnarok_game.rb
@@ -2,8 +2,12 @@ require 'artii'
 require 'colorize'
 require 'ruby-progressbar'
 
+
 input_name = true
 job_selection = true
+
+$usable_item = {}
+$items = {dagger: "Combat Knife", armor: "Padded Armor"}
 
 
 welcome = Artii::Base.new :font => 'slant'
@@ -30,10 +34,12 @@ while input_name
     if name.length < 4 then puts "Please enter longer character name".red
     else
         new_line
-        puts "Welcome to Ragnarok #{name.blue}"
+        puts "Welcome to Ragnarok #{name}"
         break
     end
 end
+$name = name.blue
+
 new_line
 while job_selection
 print "Please Select Class Job | 1 = Swordsman | 2 = Acolyte | 3 = Archer : "
@@ -61,6 +67,22 @@ end
 new_line
 array = Array.new(1000)
 name = ProgressBar.create(:total =>array.size)
+
+def stash 
+    puts "-------INVENTORY------"
+    $items.each do |item, value|
+        puts "#{item} : #{value}"
+    end
+    puts "-" * 20
+end
+def usable
+    puts "-------USABLE ITEMS------".magenta
+    $usable_item.each do |item, value|
+        puts "#{item}   : #{value}".magenta
+    end
+    puts "-".magenta * 25
+end
+
 #--Loading
 puts "Entering Ragnarok World"
 new_line
@@ -69,3 +91,18 @@ name.increment
 sleep 0.001
 end
 #--Loading
+new_line
+puts "KAFRA".yellow  + ": Hello Hero! I'am Kafra, the Town Guardian before  \n#{" "*7}you begin your adventure please take this apples \n#{" "*6} and use it to restore your hit point."
+new_line
+enter = gets
+puts "#{$name}: Thank you Kafra"
+enter = gets
+puts "#{$usable_item[:apple] = 5} Apples obtained".green + " (+) added to the inventory".green
+enter=gets
+puts "KAFRA:".yellow + " Okay, now listen carefully. Our town is currently \n#{" "*7}under attack by monsters from the dungeon and we \n#{" "*7}need you to seal that dungeon immediately!!"
+enter=gets
+puts "KAFRA:".yellow + " Please help us hero, we need you!"
+enter=gets
+puts "#{$name}: Okay! i will do it and seal the dungeon"
+enter=gets
+
